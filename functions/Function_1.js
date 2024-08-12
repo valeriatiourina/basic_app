f77181f0e40f9b209f330e486546ebc5412fdda0
valeria <valeria.tiourina@mongodb.com>
2024-08-12 13:51:29 -0400

    change message to hi

--- functions/Function_1.js
+++ functions/Function_1.js
@@ -1,10 +1,9 @@
-exports = async function(arg){
+exports = async function (arg) {
   // This default function will get a value and find a document in MongoDB
-  // To see plenty more examples of what you can do with functions see: 
+  // To see plenty more examples of what you can do with functions see:
   // https://www.mongodb.com/docs/atlas/app-services/functions/
 
   // Find the name of the MongoDB service you want to use (see "Linked Data Sources" tab)
-  
 
-  return { result: "hey" };
+  return { result: "hey hey" };
 };
